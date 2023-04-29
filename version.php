fd8afa865d94b0f17c966d16b759f289272c0a3f
farbod <zamanifarbod2@gmail.com>
2023-04-29 16:45:01 +0200

    increase version

--- version.php
+++ version.php
@@ -26,11 +26,11 @@ defined('MOODLE_INTERNAL') || die;
 
 // Defining this plug-in metadata.
 $plugin->component = 'local_och5pcore';
-$plugin->release = '2.1.1-r1';
-$plugin->version = 2022032800;
+$plugin->release = '3.0-r1';
+$plugin->version = 2023042800;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires = 2020110900; // Only Moodle v3.10 and above.
 $plugin->dependencies = [
-    'tool_opencast' => 2021091200, // Opencast admin tool (with multi-instances feature).
-    'block_opencast' => 2021091200 // Opencast Video Block (with multi-series feature).
+    'tool_opencast' => 2023030100,
+    'block_opencast' => 2023030100
 ];
